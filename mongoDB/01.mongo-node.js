a5e8cded59159ce5cbc767d1a61a8e5bcfa75520
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-21 15:47:41 +0530

    mongo client

--- mongoDB/01.mongo-node.js
+++ mongoDB/01.mongo-node.js
@@ -1,3 +1,19 @@
-const {MongoClient} = require('mongodb');
-const url = 'mongodb://localhost:27017';
-const client =  new MongoClient(url);
+
+const {MongoClient} = require('mongodb')
+const url= 'mongodb://localhost:27017';
+const databaseName='e-comm'
+const client= new MongoClient(url);
+
+
+async function getData()
+{
+    let result = await client.connect();
+    db= result.db(databaseName);
+    collection = db.collection('products');
+    let data = await collection.find({}).toArray();
+    console.log(data)
+
+
+}
+
+getData();
